ce6bd75b963bb31c0791cbcc6c9e4e17a8f008e3
luukery <luuk.heesbeen@hotmail.com>
2020-10-06 11:23:22 +0200

    stuk van nieuw AI script
    zeker nog niet klaar

---
+++ Unity/Project_Arcade/Assets/Scrips/Luuk/Npc2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Npc2 : MonoBehaviour
+{
+    public bool idle;
+    public bool attack;
+    public bool weak;
+    public bool retreat;
+    public GameObject player;
+    private float cooldown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        idle = true;
+        cooldown = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position) <= 10)
+        {
+            idle = false;
+            attack = true;
+        }
+        if(weak == true && Time.time >= cooldown)
+        {
+            cooldown = Time.time + 2f;
+        }
+        
+    }
+}
