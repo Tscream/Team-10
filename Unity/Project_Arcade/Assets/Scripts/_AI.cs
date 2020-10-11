b7f447351d1d5a702fee80734d83406479b151ac
luukery <luuk.heesbeen@hotmail.com>
2020-10-11 14:51:19 +0200

    AI gedrag
    loopt naar dezelfde Y as als de speler en loopt vervolgens naar de x as van de speler. Hij gaat nog niet over van walk naar attack, maar dat moeten we samen even naar kijken.

--- Unity/Project_Arcade/Assets/Scripts/_AI.cs
+++ Unity/Project_Arcade/Assets/Scripts/_AI.cs
@@ -8,43 +8,77 @@ public class _AI : MonoBehaviour
     GameObject player;
     private float loop_cooldown;
     private bool retreat;
-
+    public bool followY;
+    public bool follow_Y;
+    
     // Start is called before the first frame update
     void Start()
     {
         aiAnim = gameObject.GetComponent<Animator>();
         player = GameObject.Find("Player");
         loop_cooldown = 2f;
+        
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(player.transform.position, transform.position) <= 10 && aiAnim.GetBool("Idle") == true) 
+        if (Vector3.Distance(player.transform.position, transform.position) <= 10)  //&& aiAnim.GetBool("Idle") == true) 
         {
             aiAnim.SetBool("Idle", false);
             aiAnim.SetBool("Walk", true);
+            //transform.position = (new Vector3(transform.position.x, otherTransform.position.y, transform.position.z));
             gameObject.GetComponent<SpriteRenderer>().flipX = true;
+
+            if (transform.position.y < player.transform.position.y) // hiermee kan de ai met de speler op dezelfde Y as komen. 
+            {
+                followY = true;
+                follow_Y = false;
+            }
+            else if (transform.position.y > player.transform.position.y)
+            {
+                follow_Y = true;
+                followY = false;
+            }
+
+
+            if(followY == true) // de ai is lager dan de speler in het scherm
+            {
+                transform.Translate(0, 0.01f, 0);
+            }
+            else if (follow_Y == true) // de ai is hoger in dan de speler in het scherm
+            {
+                transform.Translate(0, -0.01f, 0);
+            }
+
+
+            if (aiAnim.GetBool("Attack") == false && aiAnim.GetBool("Walk") == true) 
+            {
+                transform.Translate(-0.01f, 0, 0);
+            }
+
         }
 
-        if (Vector3.Distance(player.transform.position, transform.position) <= 0.1f && aiAnim.GetBool("Walk") == true)
+        if (Vector3.Distance(player.transform.position, transform.position) <= 2 && aiAnim.GetBool("Walk") == true) // als die eenmaal bij de speler is en wilt aanvallen.
         {
 
             aiAnim.SetBool("Walk", false);
             aiAnim.SetBool("Attack", true);
-        }
+        } 
 
-        if (aiAnim.GetBool("Attack") == true && Input.GetKey(KeyCode.R))
+        if (aiAnim.GetBool("Attack") == true && Input.GetKey(KeyCode.R)) // de speler verdedigd en zo komt de ai open voor een tegenaanval
         {
-            //defend
+            
             aiAnim.SetBool("Attack", false);
             aiAnim.SetBool("Weak", true);
         } 
 
         if(aiAnim.GetBool("Attack") == true)
         {
-            //hit
-            aiAnim.SetBool("Attack", false);
+            
+            aiAnim.SetBool("Attack", false); // de ai raakt de speler met zijn aanval
             Retreat();
         }
 
@@ -55,7 +89,7 @@ public class _AI : MonoBehaviour
    
         if(retreat == true)
         {
-            transform.Translate(0.1f, 0, 0);
+            transform.Translate(0.01f, 0, 0);
             if (Time.time >= loop_cooldown)
             {
                 aiAnim.SetBool("Walk", true);
@@ -74,5 +108,16 @@ public class _AI : MonoBehaviour
 
         
     }
+
+    private void FixedUpdate()
+    {
+        float y = transform.position.y;
+
+        float scale = 2 + (1 / 4.9f) * -y;
+
+        transform.localScale = new Vector3(scale, scale, scale);
+    }
+
+    
 
 }
